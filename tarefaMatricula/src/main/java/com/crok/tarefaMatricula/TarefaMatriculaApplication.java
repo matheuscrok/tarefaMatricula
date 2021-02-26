c1243204ae9be0ab5ff8506f0b492e8383369c31
crok <zenorhan@hotmail.com>
2021-02-26 18:15:38 -0300

    Tarefa de casa Matricula inicio

---
+++ tarefaMatricula/src/main/java/com/crok/tarefaMatricula/TarefaMatriculaApplication.java
@@ -0,0 +1,13 @@
+package com.crok.tarefaMatricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefaMatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefaMatriculaApplication.class, args);
+	}
+
+}
---
+++ tarefaMatricula/src/test/java/com/crok/tarefaMatricula/TarefaMatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crok.tarefaMatricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaMatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
